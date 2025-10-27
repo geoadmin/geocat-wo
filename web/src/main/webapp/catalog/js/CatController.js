7d5e063c67a9a97fa9768f7b2ff6d9a122fc3499
christophe mangeat <christophe.mangeat@camptocamp.com>
2025-10-27 17:25:20 +0100

    apply: patch grid.html and cat controller (harvested and published logos)

--- web/src/main/webapp/catalog/js/CatController.js
+++ web/src/main/webapp/catalog/js/CatController.js
@@ -109,6 +109,7 @@
               ita: "it",
               dut: "nl",
               kor: "ko",
+              pol: "pl",
               por: "pt",
               rum: "ro",
               rus: "ru",
@@ -706,25 +707,6 @@
                 tooltip: "Grid",
                 icon: "fa-th",
                 related: []
-              },
-              {
-                tplUrl:
-                  "../../catalog/components/" +
-                  "search/resultsview/partials/viewtemplates/list.html",
-                tooltip: "List",
-                icon: "fa-bars",
-                related: ["parent", "children", "services", "datasets"]
-              },
-              {
-                tplUrl:
-                  "../../catalog/components/" +
-                  "search/resultsview/partials/viewtemplates/table.html",
-                tooltip: "Table",
-                icon: "fa-table",
-                related: [],
-                source: {
-                  exclude: ["resourceAbstract*", "Org*", "contact*"]
-                }
               }
             ],
             // Optional. If not set, the first resultViewTpls is used.
@@ -914,8 +896,10 @@
             // and redirecting to the catalogue to view metadata record
             // appUrl: "https://sextant.ifremer.fr/Donnees/Catalogue",
             isSocialbarEnabled: true,
+            isDefaultContactViewEnabled: false,
             showStatusWatermarkFor: "",
             showStatusTopBarFor: "",
+            recordviewCustomMenu: [], // List of static pages identifiers to display
             showCitation: {
               enabled: false,
               // if: {'documentStandard': ['iso19115-3.2018']}
@@ -1641,6 +1625,7 @@
     "gnExternalViewer",
     "gnAlertService",
     "gnESFacet",
+    "gnFacetMetaLabel",
     function (
       $scope,
       $http,
@@ -1661,7 +1646,8 @@
       $cookies,
       gnExternalViewer,
       gnAlertService,
-      gnESFacet
+      gnESFacet,
+      gnFacetMetaLabel
     ) {
       $scope.version = "0.0.1";
       var defaultNode = "srv";
@@ -1770,7 +1756,8 @@
         swe: "Svenska",
         ukr: "українська",
         dan: "Dansk",
-        wel: "Cymraeg"
+        wel: "Cymraeg",
+        pol: "Polski"
       };
       $scope.url = "";
       $scope.gnUrl = gnGlobalSettings.gnUrl;
@@ -1786,6 +1773,7 @@
       $scope.isExternalViewerEnabled = gnExternalViewer.isEnabled();
       $scope.externalViewerUrl = gnExternalViewer.getBaseUrl();
       $scope.publicationOptions = [];
+      $scope.getFacetLabel = gnFacetMetaLabel.getFacetLabel;
 
       $http.get("../api/records/sharing/options").then(function (response) {
         $scope.publicationOptions = response.data;
